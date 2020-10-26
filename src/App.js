60644d205c734cbe5b0d3f02420e568f867931fa
Mallaury <mallaury.programmation@gmail.com>
2020-10-26 23:12:54 +0100

    first_step

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import userInfo from '../src/constants/userInfo.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1> Salut {userInfo[1].userName}</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/constants/userInfo.js
@@ -0,0 +1,15 @@
+const userInfo = [
+    {
+        userName: 'Dupont',
+        age: 16,
+        city: 'Dunkerque'
+    },
+
+    {
+        userName: 'Dupont',
+        age: 16,
+        city: 'Dunkerque'
+    }
+]
+
+export default userInfo
